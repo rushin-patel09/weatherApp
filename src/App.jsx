17fc6f3ece222f074c788ffc657d7c22d1f90f1e
RUSHIN PATEL <204936600+rushin-patel09@users.noreply.github.com>
2025-04-16 22:15:08 +0530

    impoved some glitch in safari

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import WeatherComp from "./components/WeatherComp";
 
 const App = () => {
   return (
+    <div className="bg-linear-to-b from-[#35b1ff] to-[#206a99] min-h-max h-dvh">
     <WeatherComp />
+    </div>
   );
 };
 
